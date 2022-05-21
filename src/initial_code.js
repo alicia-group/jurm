c62286f54c0332033746f68a705184ea77395396
Raul <raulrufino0@gmail.com>
2022-05-21 09:50:22 -0300

    Turn `App` a class based component

---
+++ src/initial_code.js
@@ -0,0 +1,8 @@
+
+const initial_code = `cp pos_i pos_end
+ld pos value
+clear pos
+jp pos_to_compare line_number    
+`;
+
+export default initial_code;
--- src/urm-gui/App.js
+++ src/urm-gui/App.js
@@ -1,37 +1,42 @@
+
+import React from 'react';
 import CodeMirror from '@uiw/react-codemirror';
 
 import './App.css';
 import Registers from '../jurm-module/registers';
 import RegistersComponent from './Registers.js';
+import initial_code from '../initial_code.js';
 
-const example_code = `
-cp pos_i pos_end
-ld pos value
-clear pos
-jp pos_to_compare line_number    
-`;
+export default class App extends React.Component {
+  constructor() {
+    super();
+    console.log(initial_code)
+    let regs = new Registers(0);
+    regs.set_r(0, 1);
+    regs.set_r(4, 3);
+    regs.set_r(5, 10);
+    regs.set_r(1002, 3);
+    regs.set_r(1001, 666);
+    regs.set_r(1003, 6666666);
+    this.state = {
+      regs: regs
+    };
+  }
 
-function App() {
-  let regs = new Registers(0);
-  regs.set_r(0, 1)
-  regs.set_r(4, 3)
-  regs.set_r(5, 10)
-  regs.set_r(1002, 3)
-  regs.set_r(1001, 666)
-  regs.set_r(1003, 6666666)
-  return (
-    <div className="App">
-      <RegistersComponent initial_register={0} register_length={10} regs={regs}/>
-      <CodeMirror
-        value={example_code}
-        height="200px"
-        width="80%"
-        onChange={(value, viewUpdate) => {
-          console.log('value:', value);
-        }}
-      />
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <RegistersComponent initial_register={0} register_length={10} regs={this.state.regs}/>
+        <CodeMirror
+          value={initial_code}
+          height="200px"
+          width="80%"
+          onChange={(value, viewUpdate) => {
+            console.log('value:', value);
+          }}
+        />
+      </div>
+    );
+  }
+
 }
-
-export default App;
